42465fec3b6afa913bfa394b2c4c915d4dbb01e4
01ravimishra <ravimishra4354@gmail.com>
2026-02-24 17:45:39 +0530

    added apna college patterns

---
+++ Invervet_Haif_pyramid_rotated180.java
@@ -0,0 +1,13 @@
+public class Invervet_Haif_pyramid_rotated180 {
+    public static void main(String[] args) {
+        int r=4;
+        for(int i=1;i<=r;i++){
+            for(int j=1;j<=r-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+}
